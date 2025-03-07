0d02c067c41112a5c0e903d4a64df71d68b0130e
gieemek <gm@agmarchitekci.pl>
2025-03-07 23:11:14 +0100

    Update shaddow_rule.py

--- shaddow_rule.py
+++ shaddow_rule.py
@@ -1,15 +1,11 @@
-# Regula uruchamiajaca skrypt
-# shaddow.py
-# ktory rysune plik SVG z ukladem slonca i ksiezyca w odniesieniu do domu.
-# Rysunek SVG jest wykorzystywany w Habpanelu
-
 from core.rules import rule
 from core.triggers import when
 from core.actions import LogAction, Exec
 
-@rule( "Uruchom skrypt shaddow", description = "Uruchomienie skryptu do tworzenia rysunku shaddow.svg z położeniem słońca i księżyca w odniesieniu do domu", tags = [ "shaddow" ] )
-@when( "Item Astro_Sun_Azimuth changed" )
+@rule( "Run script shaddow", description = "Run script to create SVG drawing with sun and moon positions in relation to the house", tags = [ "shaddow" ] )
+@when( "Item Sun_Azimuth changed" )
+@when( "Item Wind_Angle changed" )
 
 def runScriptToCreateSVGfile( event ) :
-	resp = Exec.executeCommandLine( Duration.ofSeconds(10), "python3", "/etc/openhab/scripts/shaddow.py", "update", str( items[ "wuWindDirAngle" ].floatValue() ) )
-	LogAction.logInfo( "rule:shaddowExe:runScriptToCreateSVGfile", "Updating shaddow.SVG file: {}", resp )
+	resp = Exec.executeCommandLine( Duration.ofSeconds(10), "python3", "/etc/openhab/scripts/shaddow.py", "update", str( items[ "Wind_Angle" ].floatValue() ) )
+	LogAction.logInfo( "rule:runScriptToCreateSVGfile", "Updating shaddow.SVG file: {}", resp )
